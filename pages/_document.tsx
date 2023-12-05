39f0933e391deece6f5979ed3e237b3d43c342b8
Pranay Yadav <pranayyadav08@gmail.com>
2023-12-05 17:06:28 +0530

    fixed crossorigin error

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,11 +6,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="True"
-          />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;700&family=Roboto+Mono:wght@500&family=Russo+One&display=swap"
             rel="stylesheet"
