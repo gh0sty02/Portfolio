be54b7bf696b53142130568f7c5da49becc60d4e
gh0sty02 <pranayyadav08@gmail.com>
2026-06-18 21:31:51 +0530

    chore: clean up final qa warnings

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/no-unescaped-entities */
 'use client';
 
 import { motion } from 'framer-motion';
--- components/skills/SkillSetModal.tsx
+++ components/skills/SkillSetModal.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/no-multi-comp */
 'use client';
 
 import {
